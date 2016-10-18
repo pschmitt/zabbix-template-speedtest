b87163339e7569690555f9c9c931694ede424230
Philipp Schmitt <philipp@schmitt.co>
2016-10-18 16:07:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Zabbix Speedtest template
+
+## Installation
+
+- Copy `speedtest.sh` to `/usr/bin`
+- Make it executable: `chmod +x /usr/bin/speedtest.sh`
+- Install the systemd service and timer:
+`cp speedtest.service speedtest.timer /etc/systemd/system`
+- Enable the timer: `systemctl enable --now speedtest.timer`
+- Import the zabbix-agent config:
+`cp speedtest.conf /etc/zabbix/zabbix_agentd.conf.d`
+- Import the `zbx_export_templates.xml` on your Zabbix server
---
+++ speedtest.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+CACHE_FILE=/tmp/speedtest.log
+LOCK_FILE=/tmp/speedtest.lock
+
+usage() {
+    echo "$(basename $0) [-c|--cached]"
+}
+
+exec {lock_fd}>"$LOCK_FILE" || exit 1
+flock -w 60 "$lock_fd" || { echo "ERROR: flock() failed." >&2; exit 1; }
+
+case "$1" in
+    -c|--cached)
+        cat "$CACHE_FILE"
+        ;;
+    -u|--upload)
+        awk '/Upload/ { print $2 }' "$CACHE_FILE"
+        ;;
+    -d|--download)
+        awk '/Download/ { print $2 }' "$CACHE_FILE"
+        ;;
+    -p|--ping)
+        awk '/Ping/ { print $2 }' "$CACHE_FILE"
+        ;;
+    *)
+        speedtest --secure --simple > "$CACHE_FILE"
+        ;;
+esac
+
+flock -u "$lock_fd"
