0ccde09090eba391f3eae3f01e3edf2cb3953008
Tim Rightnour <6556271+garbled1@users.noreply.github.com>
2020-06-14 06:29:53 -0700

    Update zbx-speedtest-debian.sh

--- zbx-speedtest-debian.sh
+++ zbx-speedtest-debian.sh
@@ -162,7 +162,7 @@ case "$ACTION" in
     get_data_timestamp
     ;;
   run)
-    if /usr/bin/timeout 5m speedtest --accept-license --accept-gdpr -f json > "${DATA_FILE}.new"
+    if timeout 5m speedtest --accept-license --accept-gdpr -f json > "${DATA_FILE}.new"
     then
       mv "${DATA_FILE}.new" "$DATA_FILE"
     fi
